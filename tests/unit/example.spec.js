b5a9127282a35296a0dd31634c736919dd45bd68
mj <mostafajamalicv@gmail.com>
2023-01-23 12:00:57 +0330

    add asynchronous tests

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,41 @@
-import {shallowMount} from '@vue/test-utils'
+import {shallowMount} from '@vue/test-utils';  // Vue Test Utils allows you to mount a component without rendering its child components
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: {msg}
+    it('renders props.msg when passed', () => {
+        const msg = 'new message'
+        const wrapper = shallowMount(HelloWorld, {
+            propsData: {msg}
+        })
+        expect(wrapper.text()).toMatch(msg)
     })
-    expect(wrapper.text()).toMatch(msg)
-  })
 });
+
+
+const Counter = {
+    template: `
+        <div>
+        <button @click="count++">Add up</button>
+        <p>Total clicks: {{ count }}</p>
+        </div>
+    `,
+    data() {
+        return {count: 0}
+    }
+}
+
+test('increments counter value on click', async () => {
+    const wrapper = shallowMount(Counter)
+    const button = wrapper.find('button')
+    const text = wrapper.find('p')
+
+    expect(text.text()).toContain('Total clicks: 0')
+
+    await button.trigger('click')
+
+    expect(text.text()).toContain('Total clicks: 1')
+
+    setTimeout(() => {
+        wrapper.destroy(); // When using either the 'mount' or 'shallowMount' methods, it is important to note that beforeDestroy and destroyed will not be triggered unless the component is manually destroyed using Wrapper.destroy()
+    }, 1000)
+})
